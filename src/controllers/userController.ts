5e4304aa709aa7491dc8a4f63af878f060ad4f14
Marcos Vinicius Angeli Costa <marcos.vcosta@viavarejo.com.br>
2024-07-31 16:02:29 -0300

    Closes #4

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,5 +2,5 @@ import { Request, Response, json } from "express";
 
 export const testRouteConfig = async (req: Request, res: Response) => {
     console.log('Parece que deu certo');
-    return res.status(201).json({ message: "Parece que deu certo a rotinha...." });
+    return res.status(201).json({ message: "Parece que deu certo a rotinha...." }); 
 }
